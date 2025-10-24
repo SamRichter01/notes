9f3c34fdd0935974d912c03284806ab949320162
RICH9794 <rich9794@stthomas.edu>
2025-10-24 13:44:14 -0500

    Beginning work on the wysiwyg editor

--- script.js
+++ script.js
@@ -4,18 +4,31 @@
  * 10/23/25
  */
 
-const KEY_SAVEDATA = 'note'
+const KEY_SAVEDATA = 'note';
 
-const noteBox = document.querySelector('#noteBox');
+/**
+ * Parentheses around the function denote is as an Immediately Invoked Function Expression (IIFE)
+ * This means that it runs as soon as it's defined. Commonly used for initialization.
+ */
+(function init(){
+    const edit = document.querySelector('#edit');
+    edit.contentWindow.document.querySelector('body').innerHtml = "Hello!";
+    // Set the document within #edit to be editable
+    edit.contentWindow.document.designMode = 'on';
+})();
 
+/**
+ * Defunct code from when I demonstrated localstorage using just a textArea.
+ * 
 // Restore data from localStorage
 (function restore() {
     let saveData = JSON.parse(localStorage.getItem(KEY_SAVEDATA)) ?? '';
-    noteBox.textContent = saveData;
+    edit.textContent = saveData;
 })();
 
 noteBox.addEventListener('input', () => {
-    let noteContent = noteBox.value;
+    let noteContent = edit.value;
     localStorage.setItem(KEY_SAVEDATA, JSON.stringify(noteContent));
     delete(noteContent);
 });
+*/
